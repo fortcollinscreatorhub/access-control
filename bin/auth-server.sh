e0c46b01872496dd544f2b20271770d50aaaf44b
Steve Undy <steve@roseundy.net>
2018-10-31 15:32:16 -0600

    First rev

--- bin/auth-server.sh
+++ bin/auth-server.sh
@@ -1,12 +1,23 @@
-#!/bin/bash
+#!/usr/local/bin/bash
+
+# Re-spawn as a background process, if we haven't already.
+if [[ "$1" != "-n" ]]; then
+    nohup "$0" -n > /dev/null 2>&1 &
+    exit $?
+fi
 
 set -e
+
 
 script_dir="$(dirname "$0")"
 app_dir="$(cd "${script_dir}"/.. && pwd)"
 
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
+echo $$ > "${app_dir}/var/pid"
+
+#export LC_ALL=C.UTF-8
+#export LANG=C.UTF-8
+export LC_ALL=am_ET.UTF-8
+export LANG=am_ET.UTF-8
 
 . "${app_dir}/venv/bin/activate"
 export FLASK_APP="${app_dir}/auth-server/auth-server.py"
